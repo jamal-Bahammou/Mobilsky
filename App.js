1c44e4760114437424abf0c6cfe092d4ee3665a3
jamal-Bahammou <jamal.bahammou@ump.ac.ma>
2020-08-12 16:08:53 +0100

    Text & BUtton

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState('Lamine');
+
+  const clickHandler = () => {
+    setName('rass l botta');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{ name }</Text>
+      <View style={styles.buttonWrapper}>
+        <Button title='UPDATE STATE' onPress={clickHandler} />
+      </View>
+      <View style={styles.buttonWrapper}>
+        <Button title='default' onPress={() => setName('Lamine')} />
+      </View>
     </View>
   );
 }
@@ -18,4 +30,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonWrapper: {
+    marginTop: 20
+  }
 });
